5e83f031dc272a582962cc77fe0233e6c024487a
Constanza Pérez <constanza.vp.o@gmail.com>
2024-04-29 16:11:30 -0400

    configuración css del formato carrito

--- src/views/Carrito.jsx
+++ src/views/Carrito.jsx
@@ -1,5 +1,9 @@
 import { useContext } from "react";
 import { PizzaContext } from "../context/PizzaContext";
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
 
 
 const Carrito = () => {
@@ -7,23 +11,33 @@ const Carrito = () => {
 
 
   return (
-    <div>
+    <>
+    <div className="carrito">
       <h2>Detalle del pedido:</h2>
-      <div className="containerCarrito">
-        {carrito.map((elem, index) => (
-          <div key={index}>
-            <div>
-              <img className="ingCarrito" src={elem.img} alt={elem.name} />
+    
+    </div>
+
+<Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+<Row className="list">
+  <Col sm={4} style={{ width: "60rem" }}>
+    <ListGroup >
+      {carrito.map((elem, index) => (
+          <ListGroup.Item action className="especificaciones" key={index}>
+            <div className="imgName">
+              <img className="imgCarrito" src={elem.img} alt={elem.name} />
               <h5>{elem.name}</h5>
             </div>
 
             <div>
               <h6>${elem.price*elem.count}</h6>
             </div>
-          </div>
+          </ListGroup.Item>
         ))}
-      </div>
-    </div>
+    </ListGroup>
+  </Col>
+</Row>
+</Tab.Container>
+</>
   );
 };
 
